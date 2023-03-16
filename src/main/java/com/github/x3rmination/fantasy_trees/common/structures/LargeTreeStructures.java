4cc9b033014bbba0b587a3d111bd84b19bccbcca
X3RMINATION <nicolaeungur@gmail.com>
2023-03-15 17:53:14 -0700

    Fixed large tree position to be based on chunk position rather than center of chunk. Should fix `isFeatureChunk` position checking

--- src/main/java/com/github/x3rmination/fantasy_trees/common/structures/LargeTreeStructures.java
+++ src/main/java/com/github/x3rmination/fantasy_trees/common/structures/LargeTreeStructures.java
@@ -1,8 +1,10 @@
 package com.github.x3rmination.fantasy_trees.common.structures;
 
+import com.github.x3rmination.fantasy_trees.FantasyTrees;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.NoiseColumn;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.GenerationStep;
@@ -15,6 +17,7 @@ import net.minecraft.world.level.levelgen.structure.pieces.PieceGenerator;
 import net.minecraft.world.level.levelgen.structure.pieces.PieceGeneratorSupplier;
 import net.minecraft.world.level.levelgen.structure.pools.JigsawPlacement;
 import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 
 import java.util.Optional;
 
@@ -44,12 +47,15 @@ public class LargeTreeStructures extends StructureFeature<JigsawConfiguration> {
         return topBlock.getFluidState().isEmpty();
     }
 
+
+
     public static Optional<PieceGenerator<JigsawConfiguration>> createPiecesGenerator(PieceGeneratorSupplier.Context<JigsawConfiguration> context) {
         if(!LargeTreeStructures.isFeatureChunk(context)) {
             return Optional.empty();
         }
-        BlockPos centerPos = context.chunkPos().getMiddleBlockPosition(0);
-        BlockPos blockPos = new BlockPos(centerPos.getX() + 24, context.chunkGenerator().getFirstOccupiedHeight(centerPos.getX(), centerPos.getZ(), Heightmap.Types.WORLD_SURFACE_WG, context.heightAccessor()) - 5, centerPos.getZ() - 24);
+
+        BlockPos centerPos = context.chunkPos().getWorldPosition();
+        BlockPos blockPos = new BlockPos(centerPos.getX() - 24, context.chunkGenerator().getFirstOccupiedHeight(centerPos.getX(), centerPos.getZ(), Heightmap.Types.WORLD_SURFACE_WG, context.heightAccessor()) - 5, centerPos.getZ() - 24);
         Optional<PieceGenerator<JigsawConfiguration>> structurePiecesGenerator =
                 JigsawPlacement.addPieces(
                   context,
